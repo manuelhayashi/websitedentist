157f2e8b20c4c85868a6a8056837442db1f23489
manuel <manuelhayashi@icloud.com>
2020-09-20 19:11:46 -0500

    modified requirements file

--- websitedentist/settings.py
+++ websitedentist/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import django_heroku
+import dj_database_url
+from decouple import Config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -36,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'websitedentist.urls'
@@ -111,3 +115,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
